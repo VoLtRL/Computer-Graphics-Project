f0a9059e2175f3b9659b0201e217675efd005f30
PoloB19 <paul.balensi@orange.fr>
2026-01-26 12:15:06 +0100

    feat(map) : added map to the scene from glb file

--- src/scene/map.cpp
+++ src/scene/map.cpp
@@ -2,7 +2,13 @@
 #include "box.h"
 #include "physicShapeObject.h"
 
+#include "model.h"  
+#include "constants.h"
+#include <glm/gtc/matrix_transform.hpp>
+
 Map::Map(Shader* shader, Node* sceneRoot) {
+    std::string mapPath = IMAGE_DIR + std::string("map.glb");
+
     Box* groundShape = new Box(shader, 50.0f, 1.0f, 50.0f); // Large flat box as ground
     PhysicShapeObject* ground = new PhysicShapeObject(groundShape, glm::vec3(0.0f, -0.5f, 0.0f)); // Position the ground at y = -0.5 to align top surface with y = 0
     groundShape->color = glm::vec3(1.0f, 1.0f, 1.0f); 
@@ -18,7 +24,19 @@ Map::Map(Shader* shader, Node* sceneRoot) {
     ground->collisionGroup = CG_ENVIRONMENT;
     ground->collisionMask = CG_PRESETS_MAP;
 
-
     sceneRoot->add(ground); // Add ground to the scene graph
-    
+
+    // Load the map model from a .glb file
+    Model* gameMap = new Model(mapPath, shader);
+
+    // Y = -2 to align the map correctly above the ground
+    glm::mat4 transform = glm::mat4(1.0f);
+    transform = glm::translate(transform, glm::vec3(0.0f, -2.0f, 0.0f));
+
+    // Added to scene
+    if (gameMap->rootNode) {
+        gameMap->rootNode->set_transform(transform);
+        gameMap->rootNode->name = "Map_Projet";
+        sceneRoot->add(gameMap->rootNode);
+    }
 }
